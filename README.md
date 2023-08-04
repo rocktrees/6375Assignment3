042e801c013f70badbaca753cdcf9d737272b300
rocktrees <imovewaves@gmail.com>
2023-08-03 21:58:15 -0500

    plotting, refactor

--- README.md
+++ README.md
@@ -9,11 +9,13 @@ hxy170003, kml190002
 
 please run in command line.
 
-1. For a default run **WITHOUT** saving log, use `python kmeans.py`
+1. For a default run **WITHOUT** saving log, use `python run.py`
    1. This defaults to `k=10` where k is the number of clusters
 2. If you want to try a different k, then use the flag `--k <# of clusters>`
-   1. ex: `python kmeans.py --k 5`
+   1. ex: `python run.py --k 5`
 3. If you would like to keep a `log.txt`, then use the flag `--log True`
    1. This will run k times. Once for each k to 1
    2. For each run, it will save the `sse` and `cluster breakdown` in a text file called `log.txt`
-   3. ex: `python kmeans.py --k 5 --log True`
+   3. At the end, it will save a plot of the SSE vs Clusters onto the current directory
+      1. For the elbow method
+   4. ex: `python run.py --k 5 --log True`
---
+++ utils/util.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import re
+import pandas as pd
+
+def cleanData(file_path):
+    
+    df = pd.read_csv(file_path, header=None, sep="|")
+    tweets = list(df[2])
+
+    cleaned = [tweetFix(tweet) for tweet in tweets]
+    return cleaned
+
+
+def tweetFix(tweet):
+    s = re.sub(r'@\w+|http://\S+', '', tweet)
+    s = re.sub(r'RT.*?:', '', s)
+    s = s.replace(":", "")
+    s = s.replace("#", "")
+    s = s.lower()
+
+    return s
+
+def logClusters(i, k, sse, clusters):
+
+    if i == 0:
+        # if log_file already exists, then delete it
+        log_file = Path("log.txt")
+        if log_file.is_file():
+            log_file.unlink()
+
+        with open("log.txt", "a") as file:
+            file.write(f"Kmeans log\n")
+            file.write(f"--------------------------------------------------\n")
+            file.write(f"--------------------------------------------------\n")
+
+    with open("log.txt", "a") as file:
+        file.write(f"\nK: {k}\tSSE: {sse}\n")
+        file.write(f"Size of each cluster\n")
+        for i in range(k):
+            file.write(f"\tcluster {(i+1)}: {len(clusters[i])} tweets\n")
+
+    return sse
